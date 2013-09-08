ea8c7c0a15bab154d7a4692912a2373c40a4f97b
James Wenzel <jcw@jcwenzeldesign.com>
2013-09-08 18:10:04 -0500

    adding script to tell crap browsers they are out of date

--- js/main.js
+++ js/main.js
@@ -46,3 +46,13 @@ $(document).ready(function() {
   itemsMobile : false
   });
 });
+
+var $buoop = {vs:{i:8,f:20,o:10.6,s:5,n:9}} 
+$buoop.ol = window.onload; 
+window.onload=function(){ 
+  try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
+  var e = document.createElement("script"); 
+  e.setAttribute("type", "text/javascript"); 
+  e.setAttribute("src", "http://browser-update.org/update.js"); 
+  document.body.appendChild(e); 
+} 
