7dafd889819f0665ab8ac5a214f1a8d36dffd686
James Wenzel <jcw@jcwenzeldesign.com>
2013-09-15 13:02:26 -0500

    Reformatted into one document.ready function. Formatted  object to be more readable and try/catch. Chained jQuery selectors into one for the menu & overlay
    Yours truly,
    Ajlouie

--- js/main.js
+++ js/main.js
@@ -1,56 +1,65 @@
-// responsive nav
-var navigation = responsiveNav("#nav-js", {
-	animate: true,        // Boolean: Use CSS3 transitions, true or false
-	transition: 400,      // Integer: Speed of the transition, in milliseconds
-	label: "Menu",        // String: Label for the navigation toggle
-	insert: "after",      // String: Insert the toggle before or after the navigation
-	customToggle: "",     // Selector: Specify the ID of a custom toggle
-	openPos: "relative",  // String: Position of the opened nav, relative or static
-	jsClass: "js",        // String: 'JS enabled' class which is added to <html> el
-	init: function(){},   // Function: Init callback
-	open: function(){},   // Function: Open callback
-	close: function(){}   // Function: Close callback
-});
-
-// owl carousel
+// Document Ready Function & owlCarousel
 $(document).ready(function() {
   $('#m-carousel').owlCarousel({
-  baseClass: 'm-carousel',
-  theme: 'm-carousel',
+    baseClass: 'm-carousel',
+    theme: 'm-carousel',
 
-  autoPlay: false,
-	slideSpeed : 600,
-  paginationSpeed : 400,
+    autoPlay: false,
+    slideSpeed : 600,
+    paginationSpeed : 400,
 
-  items : 1,
-  itemsDesktop : false,
-  itemsDesktopSmall : false,
-  itemsTablet: false,
-  itemsMobile : false
+    items : 1,
+    itemsDesktop : false,
+    itemsDesktopSmall : false,
+    itemsTablet: false,
+    itemsMobile : false
   });
-});
-
-$(document).ready(function() {
   $('#m-carousel--full').owlCarousel({
-  baseClass: 'm-carousel--full',
-  theme: 'm-carousel--full',
+    baseClass: 'm-carousel--full',
+    theme: 'm-carousel--full',
 
-  autoPlay: false,
-  slideSpeed : 600,
-  paginationSpeed : 400,
+    autoPlay: false,
+    slideSpeed : 600,
+    paginationSpeed : 400,
 
-  items : 1,
-  itemsDesktop : false,
-  itemsDesktopSmall : false,
-  itemsTablet: false,
-  itemsMobile : false
+    items : 1,
+    itemsDesktop : false,
+    itemsDesktopSmall : false,
+    itemsTablet: false,
+    itemsMobile : false
   });
 });
 
-var $buoop = {vs:{i:8,f:20,o:10.6,s:5,n:9}} 
+// responsive nav
+var navigation = responsiveNav("#nav-js", {
+  animate: true,        // Boolean: Use CSS3 transitions, true or false
+  transition: 400,      // Integer: Speed of the transition, in milliseconds
+  label: "Menu",        // String: Label for the navigation toggle
+  insert: "after",      // String: Insert the toggle before or after the navigation
+  customToggle: "",     // Selector: Specify the ID of a custom toggle
+  openPos: "relative",  // String: Position of the opened nav, relative or static
+  jsClass: "js",        // String: 'JS enabled' class which is added to <html> el
+  init: function(){},   // Function: Init callback
+  open: function(){},   // Function: Open callback
+  close: function(){}   // Function: Close callback
+});
+
+var $buoop = {
+  vs:{ //Control Version Number of Browser
+    i:8,    // IE 8- (Why would you do this?)
+    f:20,   // Firefox 20-(Butterface)
+    o:10.6, // Opera 10.6-(As boring as real Opera)
+    s:5,    // Safari 5-(Meh...)
+    n:9     // Netscape 9(You must really hate yourself)
+  }
+} 
 $buoop.ol = window.onload; 
 window.onload=function(){ 
-  try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
+  try {
+    if ($buoop.ol) 
+      $buoop.ol();
+  }
+  catch (e) {} 
   var e = document.createElement("script"); 
   e.setAttribute("type", "text/javascript"); 
   e.setAttribute("src", "http://browser-update.org/update.js"); 
@@ -59,14 +68,12 @@ window.onload=function(){
 
 $('#menu-corporate').on({
   click: function(){
-    $('.m-overlay-wrapper').removeClass('s-is-hidden');
-    $('#_corporate-menu').removeClass('s-is-hidden');
+    $('#_corporate-menu, .m-overlay-wrapper').removeClass('s-is-hidden');
   }
 });
 
 $('.m-overlay').find('.btn--close').on({
   click: function(){
-    $('.m-overlay-wrapper').addClass('s-is-hidden');
-    $('#_corporate-menu').addClass('s-is-hidden');
+    $('#_corporate-menu, .m-overlay-wrapper').addClass('s-is-hidden');
   }
 });
